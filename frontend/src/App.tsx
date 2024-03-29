b2692d459d2b5f4a70bd1c74eb0d886c49c50bc6
Zain Akram <zainakram2018@gmail.com>
2024-03-29 15:57:23 -0700

    feat: Add React Router DOM and authentication context
    - Implement basic authentication context with AuthProvider and useAuth hook
    - Create private route component for authenticated routes
    - Add LoginPage and HomePage components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import PrivateRoute from "./routes/PrivateRoutes";
+import Login from "./pages/LoginPage";
+import Home from "./pages/HomePage";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App: React.FC = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />} />
 
-export default App
+          <Route path="/" element={<PrivateRoute />}>
+            <Route path="/account" element={<Home />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+};
+
+export default App;
